1969a5f40a78772660fee28c0cd6e6f2ce6e6b6d
Sukhvsin2 <sukhvsingh2026@gmail.com>
2021-01-23 15:34:44 -0600

    Port updated

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const cors = require('cors')
-const route = require('./routes/mapRoutes')
+const route = require('./routes/mapRoutes');
+const e = require('express');
 
 const app = express()
 
 app.use(cors());
 app.use(route);
 
-app.listen(2727, () => {
+const port = 2727 || process.env.PORT
+
+app.listen(port, () => {
     console.log('Server running at 2727');
  })
