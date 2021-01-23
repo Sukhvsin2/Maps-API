8af6f5c9513f06a49d13e1e71d4825838c23221b
Sukhvsin2 <sukhvsingh2026@gmail.com>
2021-01-23 15:17:22 -0600

    Api Working

---
+++ mapPoints/specificPoints.js
@@ -0,0 +1,6 @@
+const locate = {
+    'Chandigarh': [{ 'latitude': 30.7691, 'longitude': 76.5759 }],
+    'NewDelhi': [[]]
+}
+
+module.exports = locate;
--- routes/mapRoutes.js
+++ routes/mapRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const Router = express.Router()
 const points = require('../mapPoints/points')
+const specificPoints = require('../mapPoints/specificPoints')
 
 Router.get('/', (req, res) => {
     res.status(200).json({points})
 })
  
 Router.get('/:location', (req, res) => {
-    console.log(req.params.location);
-    res.status(200).json({'data': 'check'})
+    const data = specificPoints[req.params.location];
+    res.status(200).json({'data': data})
 })
 
 module.exports = Router;
