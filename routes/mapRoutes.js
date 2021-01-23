5c1f0d6d0fb042cc2254c06278173fee6caa3cfd
Sukhvsin2 <sukhvsingh2026@gmail.com>
2021-01-23 11:19:47 -0600

    packges added

--- routes/mapRoutes.js
+++ routes/mapRoutes.js
@@ -4,6 +4,11 @@ const points = require('../mapPoints/points')
 
 Router.get('/', (req, res) => {
     res.status(200).json({points})
- })
+})
+ 
+Router.get('/:location', (req, res) => {
+    console.log(req.params.location);
+    res.status(200).json({'data': 'check'})
+})
 
 module.exports = Router;
