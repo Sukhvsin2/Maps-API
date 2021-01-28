d7cb9a4e66f68e096b91e0e2ccf27f75ac8bd527
Sukhvsin2 <sukhvsingh2026@gmail.com>
2021-01-28 00:08:01 -0600

    Points updated

--- mapPoints/specificPoints.js
+++ mapPoints/specificPoints.js
@@ -1,7 +1,10 @@
 const locate = {
     'Chandigarh': [{ 'latitude': 30.7691, 'longitude': 76.5759, 'message': 'University' }, { 'latitude': 30.6425, 'longitude': 76.8173, 'message': 'Zirakpur' }],
-    'NewDelhi': [],
-    'India': [{ 'latitude': 28.7041, 'longitude': 77.1025, 'message': 'Home' }, { 'latitude': 30.7333, 'longitude': 76.7794, 'message': 'Chandigarh' }, { 'latitude': 29.0588, 'longitude': 76.0856, 'message': 'Haryana' }, { 'latitude': 31.6340, 'longitude': 74.8723, 'message': 'Amritsar' }, {'latitude': 26.8467, 'longitude': 80.9462, 'message': 'UP'}]
+    'NewDelhi': [{'latitude': 28.6296, 'longitude': 77.0524, 'message': 'School'}],
+    'India': [{ 'latitude': 28.7041, 'longitude': 77.1025, 'message': 'Home' }, { 'latitude': 30.7333, 'longitude': 76.7794, 'message': 'Chandigarh' }, { 'latitude': 29.0588, 'longitude': 76.0856, 'message': 'Haryana' }, { 'latitude': 31.6340, 'longitude': 74.8723, 'message': 'Amritsar' }, {'latitude': 26.8467, 'longitude': 80.9462, 'message': 'UP'}],
+    'LifeMap': [{'latitude': 28.618534088134766, 'longitude': 77.0567855834961, 'message': 'Home'}, {'latitude': 28.6296, 'longitude': 77.0524, 'message': 'School'}],
+    'USA': [{ 'latitude': 41.978611, 'longitude': -87.904724, 'message': 'Chicago' }, { 'latitude': 29.9902, 'longitude': 95.3368, 'message': 'Texas' }, { 'latitude': 34.6403, 'longitude': 86.7757, 'message': 'HuntsVille' }, { 'latitude': 43.8087, 'longitude': 11.2012, 'message': 'Florence' }],
+    "wish": [{ 'latitude': 28.3852, 'longitude':  81.5639, 'message': 'DisneyLand' }, { 'latitude': 37.6485, 'longitude': 118.9721, 'message': 'Mommoth Lake' }, { 'latitude': 36.2710, 'longitude':  136.8986,'message': 'Shirakawago'}]
 }
 
 module.exports = locate;
